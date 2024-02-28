9639b8631b3c823f10464d9f0d87082383938aec
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-02-28 21:01:49 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,14 +15,6 @@ const Skills = () => {
               <img className='icon' src={skill.icon} alt={skill.name} />
             </div>
           ))}
-          {/* <div className=" skillset">
-            <h5>GitHub</h5>
-            <img className='icon' src={git} alt="GitHub" />
-          </div>
-          <div className="skill10" >
-            <h4>Framer Motion</h4>
-            <img className='icon framerIcon' src={framer} alt="framer" />
-          </div> */}
         </div>
       </div>
     </>
