45a51e2015b342b708170114b390c95dc581c110
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-04-18 13:44:29 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { skillData } from '../assets/data.js';
+import {motion} from "framer-motion";
+import { listAnimationVariants, slideInFromTop } from './Animations.js';
 
 const Skills = () => {
   return (
     <>
       <div className="skillsSection">
-        <div className="sectionHeader">
+        <motion.div className="sectionHeader"
+          variants={slideInFromTop(-45, 0.5)}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+        >
           <h2>Skills</h2>
-        </div>
+        </motion.div>
         <div id="skills">
           {skillData.map((skill, index) => (
-            <div key={index} className={`skill${index + 1}`}>
+            <motion.div key={index} className={`skill${index + 1}`}
+              variants={listAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
               <h5>{skill.name}</h5>
               <img className='icon' src={skill.icon} alt={skill.name} />
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
