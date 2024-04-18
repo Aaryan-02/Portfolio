8b6167608887bc4570cf39fc1a720c7b503c910e
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-04-18 13:35:34 +0530

    Update Animations.js

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -1,5 +1,105 @@
+import { easeInOut } from 'framer-motion';
 import gsap from "gsap";
 
+// Framer Motion Animations
+export const slideInFromLeft = (initialX, delay) => {
+  return {
+    initial: { x: initialX, opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+        ease: easeInOut,
+      },
+    },
+  };
+}
+
+export const slideInFromRight = (initialX, delay) => {
+  return {
+    initial: { x: initialX, opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+        ease: easeInOut,
+      },
+    },
+  };
+}
+
+export const slideInFromTop = (initialY, delay) => {
+  return {
+    initial: { y: initialY, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+        ease: easeInOut,
+      },
+    },
+  };
+}
+
+export const slideInFromBottom = (initialY, delay) => ({
+  initial: {
+    opacity: 0,
+    y: initialY,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: delay,
+      ease: easeInOut,
+    }
+  }
+})
+
+export const imgVariant = (initialX) => ({
+  initial: {
+    x: initialX,
+    opacity: 0,
+    scale: 0.8,
+    color: 'transparent',
+    textShadow: '0px 0px 8px rgba(0,0,0,0.5)',
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    color: '#333',
+    textShadow: '0px 0px 0px rgba(0,0,0,0)',
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+      ease: easeInOut,
+    }
+  }
+});
+
+export const listAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05*index,
+      ease: easeInOut,
+    },
+  }),
+}
+
+// GSAP Animations
 // OPEN MENU
 export const staggerReveal = (node1, node2) => {
   gsap.from([node1, node2], {
