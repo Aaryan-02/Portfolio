6241688bb5fe9e60bf44a932b1e2a14aa99e2f3c
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-04-18 13:35:11 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,52 +1,66 @@
-import React, { useEffect, useRef } from 'react';
-import { animate, motion } from "framer-motion";
+import React from 'react';
 import Timeline from './Timeline';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { motion } from 'framer-motion';
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from './Animations.js';
 
 
 const About = () => {
-    const projectCount = useRef(0);
-
-    // // var element = projectCount.current;
-
-    // const animationProjectsCount = () => {
-    //     animate(0, 30, {
-    //         duration: 1,
-    //         onUpdate: (v) => (projectCount.current.textContent = v.toFixed())
-    //     })
-    //     console.log(projectCount.current)
-    // }
-
-    useEffect(()=>{
-        Aos.init({duration: 2000, once: true})
-    }, []);
 
     return (
         <div id='about'>
             <div className="abtSections">
-                <div className="sectionHeader" data-aos="zoom-in" data-offset='80'>
+                <motion.div className="sectionHeader" variants={slideInFromTop(-45, 0.5)}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{
+                        once: true,
+                    }}
+                >
                     <h2>About</h2>
+                </motion.div>
+                <div className="abtLeft" >
+                    <motion.p
+                        variants={slideInFromLeft(-20, 0.5)}
+                        initial="initial"
+                        whileInView="animate"
+                        viewport={{
+                            once: true,
+                        }}
+                    >
+                        Dynamic and enthusiastic fresher with a strong foundation in software development, aiming to secure a position as a Developer. Eager to apply my programming skills and problem-solving abilities to develop efficient and reliable software solutions. Seeking an opportunity to learn from experienced professionals, contribute to the team's goals, and enhance my technical expertise in a progressive organization.
+                    </motion.p>
                 </div>
-                <div className="abtLeft" data-aos="zoom-in-right">
-                    <p>Dynamic and enthusiastic fresher with a strong foundation in software development, aiming to secure a position as a Developer. Eager to apply my programming skills and problem-solving abilities to develop efficient and reliable software solutions. Seeking an opportunity to learn from experienced professionals, contribute to the team's goals, and enhance my technical expertise in a progressive organization.</p>
-                </div>
-                <div className="abtRight" data-aos="zoom-in-left">
-                    <div className="eduExp">
+                <div className="abtRight" >
+                    <motion.div className="eduExp" variants={slideInFromRight(40, 0.5)} initial="initial"
+                        whileInView="animate"
+                        viewport={{
+                            once: true,
+                        }}
+                    >
                         <h2>Education</h2>
                         <h2>Experience</h2>
-                    </div>
+                    </motion.div>
                     <Timeline />
                 </div>
                 <div style={{ clear: "both" }}></div>
             </div>
-            <div className='projectsDone' data-aos="fade-up">
+            <motion.div className='projectsDone' variants={slideInFromLeft(-45, 0.7)} initial="initial"
+                whileInView="animate"
+                viewport={{
+                    once: true,
+                }}
+            >
                 <p className='number'>
                     +<span>30</span>
                 </p>
                 <span className='done'>Projects Done</span>
-            </div>
-            <div className="personal-info" data-aos="fade-up">
+            </motion.div>
+            <motion.div className="personal-info" variants={slideInFromLeft(-45, 0.8)} initial="initial"
+                whileInView="animate"
+                viewport={{
+                    once: true,
+                }}
+            >
                 <div className="row">
                     <div className="info-item">
                         <p>Age: <span>20</span></p>
@@ -63,7 +77,7 @@ const About = () => {
                         <p>Email: <span> imaryan32@gmail.com</span></p>
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
