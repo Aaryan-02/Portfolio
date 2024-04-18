e6a39454b61c09597dc9bc70e75975ae6d52161b
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-04-18 13:43:42 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { motion } from "framer-motion";
+import { slideInFromLeft, slideInFromTop, slideInFromRight } from './Animations.js';
 import $ from 'jquery';
 import { ProjectData } from '../assets/data.js';
+import { FaArrowCircleDown } from "react-icons/fa";
 
 export function ProjectCard({ imageSrc, title, description, visitURL }) {
   return (
@@ -13,8 +16,7 @@ export function ProjectCard({ imageSrc, title, description, visitURL }) {
               <h3 className="projectTitle">{title}</h3>
               <p dangerouslySetInnerHTML={{ __html: description }}></p>
               <div className='proLinks'>
-                <a className='visit' href={visitURL} target="_blank">Visit Project</a>
-                {/* <Link className='iconLink' href="https://github.com/Aaryan-02"><FaGithub size={'2em'} /></Link> */}
+                <a className='visit' href={visitURL} target="_blank" rel="noreferrer">Visit Project</a>
               </div>
             </aside>
           </div>
@@ -51,23 +53,60 @@ const Projects = () => {
     })
 
   };
+  
   // Jquery and React don't go well together since both are working on DOM and React overshadows the DOM. So to make use of it we have to simply use useEffect hook and with this the jQuery function would be called on load
   useEffect(() => {
     $('.cards').commentCards();
   })
 
+
   return (
     <>
       <div id="projectSection">
-        <div className="sectionHeader">
+        <motion.div className="sectionHeader"
+          variants={slideInFromTop(-45, 0.5)}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+        >
           <h2>Projects</h2>
-        </div>
+        </motion.div>
+        <motion.div className='slidingText' 
+          variants={slideInFromRight(45, 0.5)}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+        >
+          <div class="wrapper">
+            <div class="marquee">
+              <p> Click on the Next Card. </p>
+              <p> Click on the Next Card. </p>
+              <p> Click on the Next Card. </p>
+              <p> Click on the Next Card. </p>
+              <p> Click on the Next Card. </p>
+            </div>
+          </div>
+          <div className='downArrow'>
+            <span><FaArrowCircleDown /></span>
+          </div>
+        </motion.div>
         <div id="projects">
-          <ul className="cards" >
-              {ProjectData.map((item) => (
-                <ProjectCard key={item.title} {...item}/>
-              ))}
-          </ul>
+          <motion.ul className="cards"
+            variants={slideInFromLeft(-45, 0.5)}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+          >
+            {ProjectData.map((item) => (
+              <ProjectCard key={item.title} {...item} />
+            ))}
+          </motion.ul>
         </div>
       </div>
     </>
